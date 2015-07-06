a24df53a446618aeef904c54a70b59005a507093
Gim Gyoung Jin <stepping1st@gmail.com>
2015-07-06 17:32:06 +0900

    add jsr 303 bean validator test

--- rest-api-sample/src/test/java/com/demo/validation/ValidationTest.java
+++ rest-api-sample/src/test/java/com/demo/validation/ValidationTest.java
@@ -1,34 +1,50 @@
 package com.demo.validation;
 
-import static org.junit.Assert.assertThat;
+import static org.fest.assertions.Assertions.assertThat;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.mock.web.MockServletConfig;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
 
+import com.demo.RestApiSampleApplication;
+import com.demo.domain.Article;
+import com.demo.validator.ProfileValidatorFactoryBean;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RestApiSampleApplication.class)
+@WebAppConfiguration
 public class ValidationTest {
 
+	static Logger logger = Logger.getLogger(ValidationTest.class);
+	
+	@Autowired
+	@Qualifier("ProfileValidatorFactoryBean")
+	private ProfileValidatorFactoryBean validator;
+	
     @Test
     public void testJsr303BeanValidator() throws ServletException, IOException {
-//        ConfigurableDispatcherServlet servlet = new ConfigurableDispatcherServlet();
-//        servlet.setRelativeLocations(getClass(), "jsr-303-validation.xml");
-//        servlet.init(new MockServletConfig("dispatcherServlet"));
-// 
-//        MockHttpServletRequest req = new MockHttpServletRequest("POST", "/user");
-//        req.addParameter("id", "rednics"); // not null, min 4, max 10
-//        req.addParameter("name", "신관영"); // not null, min 10
-//        req.addParameter("password", "1qaz1234"); // not null, min 6, max 20
-//        req.addParameter("age", "39"); // min 1, max 200
-// 
-//        MockHttpServletResponse res = new MockHttpServletResponse();
-//        servlet.service(req, res);
-// 
-//        assertThat(servlet.getModelAndView().getViewName(), is("successView"));
+    	
+    	Article article = new Article();
+    	BindException errors = new BindException(article, "contents");
+    	validator.validate(article, errors);
+    	List<ObjectError> allErrors = errors.getAllErrors();
+    	
+    	assertThat(allErrors.size()).isGreaterThan(0);
+    	
+    	logger.debug(allErrors);
+    	
     }
 
 }
